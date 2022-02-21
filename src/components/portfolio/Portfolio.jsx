8ecf4f3259ffe25042efe9fefc62cfaf05537e40
Abdoul Fadel <bidigafadel@gmail.com>
2022-02-21 02:02:10 +0000

    portfolio section bêta, card and slide done

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,6 +1,7 @@
 import "./portfolio.scss"
-import { useContext } from "react"
+import { useContext, useState } from "react"
 import { ThemeContext } from "../../context"
+import { ArrowBackIos, ArrowForwardIos } from "@material-ui/icons";
 
 export default function Portfolio() {
   const theme = useContext(ThemeContext)
@@ -8,35 +9,64 @@ export default function Portfolio() {
   const handleClick = ()=>{
       theme.dispatch({type: "TOGGLE"})
   }
+
+  const [currentSlide, setCurrentSlide] = useState(1)
+
+  const data = [
+    {
+      'id': 1,
+      'title' : 'Machine Learnine: OCR',
+      'desccription' : 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, aspernatur. Voluptatum adipisci, repellat placeat eveniet incidunt delectus dolor deleniti iste esse in blanditiis magni corrupti saepe, aliquid voluptates! Reiciendis, cumque.',
+      'technos': ['Python', 'Pandas', 'Pytorch'],
+      'img': 'assets/code.png'
+    },
+    {
+      'id': 2,
+      'title' : 'Application de gestion',
+      'desccription' : 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, aspernatur. Voluptatum adipisci, repellat placeat eveniet incidunt delectus dolor deleniti iste esse in blanditiis magni corrupti saepe, aliquid voluptates! Reiciendis, cumque.',
+      'technos': ['ReactJs', 'FastApi', 'Python', 'Javascript'],
+      'img': 'assets/code.png'
+    },
+    {
+      'id': 3,
+      'title' : 'Application mobile',
+      'desccription' : 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, aspernatur. Voluptatum adipisci, repellat placeat eveniet incidunt delectus dolor deleniti iste esse in blanditiis magni corrupti saepe, aliquid voluptates! Reiciendis, cumque.',
+      'img': 'assets/code.png',
+      'technos': ['Python', 'Pandas', 'Pytorch']
+    }
+  ]
+  const handle = (val) => {
+     val === "left" ? setCurrentSlide(currentSlide > 1 ?  currentSlide-1 : data.length) : setCurrentSlide(currentSlide < data.length ? currentSlide + 1 : 1)
+  }
   return (
       <div className="Portfolio" id="portfolio">
         <h1>Portfolio</h1>
-        <div className="slider">
-          <div className="container">
+        <div className="slider" style={{transform: `translateX(-${currentSlide * 100}vw)`}}>
+          {data.map(item => ( <div className="container">
             <div className="item" style={{backgroundColor: theme.state.darkMode ? "white" : "black", color: theme.state.darkMode ? "black" : "white"}}>
               <div className="left">
                 <div className="imgcontainer">
-                  <img src="assets/man.png" alt="" srcset="" />
+                  <img src={item.img} alt="" srcset="" className="projet"/>
                 </div>
               </div>
               <div className="right">
                 <div className="title">
-                  Machine Learning: OCR
+                  {item.title}
                 </div>
                 <div className="description">
-                  Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsam, obcaecati. Explicabo, deleniti assumenda reprehenderit dolor hic ipsum accusamus, fuga atque, ullam voluptatum possimus reiciendis saepe consectetur aspernatur cumque et iure?
+                  {item.desccription}
                 </div>
                 <div className="technos">
                   <ul>
-                    <li>Python</li>
-                    <li>Pytorch</li>
-                    <li>Pandas</li>
+                    {item.technos.map( i => (<li>{i}</li>))}
                   </ul>
                 </div>
               </div>
             </div>
-          </div>
-        </div>        
+          </div>))}
+        </div>
+        <ArrowBackIos  className="arrow left" onClick={()=>handle("left")} style={{color: theme.state.darkMode ? "white" : "black"}}/>        
+        <ArrowForwardIos className="arrow right" onClick={()=>handle("right")} style={{color: theme.state.darkMode ? "white" : "black"}}/>        
       </div>
   )
 }
