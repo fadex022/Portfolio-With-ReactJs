0ff05208fc87740f05edb16b3001d11e1202330a
Abdoul Fadel <bidigafadel@gmail.com>
2022-03-16 09:54:49 +0000

    testimonials beta done

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,5 +1,5 @@
 import "./testimonials.scss"
-import { Twitter, YouTube } from "@material-ui/icons";
+import { Twitter, Instagram, Facebook, LinkedIn } from "@material-ui/icons";
 import { useContext, useState } from "react"
 import { ThemeContext } from "../../context"
 
@@ -7,24 +7,64 @@ import { ThemeContext } from "../../context"
 export default function Testimonials() {
   const theme = useContext(ThemeContext)
 
+  const data = [
+    {
+      id: 1,
+      name: 'Lecam Papa',
+      message: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae ut, excepturi nemo odio perferendis sapiente vero.',
+      job: 'Flutter Dev',
+      twitter: 'link',
+      facebook: 'link',
+      instagram: 'link',
+      linkedin: 'link',
+      image: 'assets/man.png'
+    },
+    {
+      id: 2,
+      name: 'Kouame Auguinard',
+      message: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae ut, excepturi nemo odio perferendis sapiente vero.',
+      job: 'Fullstack and Devops',
+      twitter: 'link',
+      facebook: '',
+      instagram: 'link',
+      linkedin: 'link',
+      image: 'assets/man.png'
+    },
+    {
+      id: 1,
+      name: 'Lecam Papa',
+      message: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae ut, excepturi nemo odio perferendis sapiente vero.',
+      job: 'Flutter Dev',
+      twitter: 'link',
+      facebook: 'link',
+      instagram: 'link',
+      linkedin: '',
+      image: 'assets/man.png'
+    }
+  ]
+
   return (
       <div className="Testimonials" id="testimonials">
         <h1>Témoignages</h1>
         <div className="container">
+          {data.map(item => (
           <div className="card">
             <div className="top">
               <img src="assets/man.png" alt="profile picture" className="user" />  
               <div className="social">
-                <Twitter  className="item" style={{color: theme.state.darkMode ? "white" : "black"}}/>     
-                <YouTube  className="item"  style={{color: theme.state.darkMode ? "white" : "black"}}/>  
+                {item.twitter ? <Twitter  className="item" style={{color: theme.state.darkMode ? "white" : "black"}}/> : null}
+                {item.instagram ?<Instagram  className="item"  style={{color: theme.state.darkMode ? "white" : "black"}}/> : null}
+                {item.facebook ?<Facebook  className="item"  style={{color: theme.state.darkMode ? "white" : "black"}}/> : null}
+                {item.linkedin ?<LinkedIn  className="item"  style={{color: theme.state.darkMode ? "white" : "black"}}/> : null}
               </div>   
             </div>
-            <div className="center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae ut, excepturi nemo odio perferendis sapiente vero.</div>
+            <div className="center">{item.message}</div>
             <div className="bottom">
-              <h3>Lecam</h3>
-              <h4>Senior dev</h4>
+              <h3>{item.name}</h3>
+              <h4>{item.job}</h4>
             </div>
           </div>
+          ))}
         </div>
       </div>
   )
